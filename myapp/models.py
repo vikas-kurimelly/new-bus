ab8621a823e87e7faefe35c3eb49345a774e97a2
nampallyharish4 <nampallyharish5544@gmail.com>
2024-12-24 12:34:29 +0530

    changes in admin

--- myapp/models.py
+++ myapp/models.py
@@ -3,15 +3,34 @@ from django.db import models
 
 
 class Bus(models.Model):
+    TELANGANA_CITIES = [
+        ('Hyderabad', 'Hyderabad'),
+        ('Warangal', 'Warangal'),
+        ('Nizamabad', 'Nizamabad'),
+        ('Khammam', 'Khammam'),
+        ('Karimnagar', 'Karimnagar'),
+        ('Ramagundam', 'Ramagundam'),
+        ('Mahbubnagar', 'Mahbubnagar'),
+        ('Nalgonda', 'Nalgonda'),
+        ('Adilabad', 'Adilabad'),
+        ('Suryapet', 'Suryapet'),
+    ]
+
     id = models.BigAutoField(primary_key=True)
-    bus_name = models.CharField(max_length=30)
-    source = models.CharField(max_length=30)
-    dest = models.CharField(max_length=30)
-    nos = models.DecimalField(decimal_places=0, max_digits=2)
+    BUS_NUMBERS = [(f'TG 76Z {str(i).zfill(4)}{" Express" if i in range(1, 6) else " Deluxe" if i in range(6, 11) else " Super Luxury" if i in range(11, 16) else " EV Bus" if i in range(15, 21) else ""}', f'TG 76Z {str(i).zfill(4)}{" Express" if i in range(1, 6) else " Deluxe" if i in range(6, 11) else " Super Luxury" if i in range(11, 16) else " EV Bus" if i in range(15, 21) else ""}') for i in range(1, 21)]
+    bus_name = models.CharField(max_length=30, choices=BUS_NUMBERS)
+    source = models.CharField(max_length=30,choices=TELANGANA_CITIES)
+    dest = models.CharField(max_length=30, choices=TELANGANA_CITIES)
+    SEAT_CHOICES = [
+        (40, '40'),
+        (50, '50'),
+        (60, '60'),
+    ]
+    nos = models.IntegerField(choices=SEAT_CHOICES)
     rem = models.DecimalField(decimal_places=0, max_digits=2)
     price = models.DecimalField(decimal_places=2, max_digits=6)
     date = models.DateField()
-    time = models.CharField(max_length=10)  # Changed to CharField for dd/mm/yyyy format
+    time = models.TimeField()
     
     class Meta:
         verbose_name_plural = "List of Busses"
